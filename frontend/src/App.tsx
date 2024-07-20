afeed2757d2724cb40aff148d8508e3a072b5ca5
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2024-07-20 16:15:08 +0900

    create discription display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { MouseEventHandler, useEffect, useState } from 'react'
 
 import './App.css'
 import { Todo } from "./types/Todo"
@@ -6,12 +6,14 @@ import { getTodos } from  "./module/api"
 
 const App = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [discriptionState, setDiscriptionState] = useState<Boolean[]>([]);
 
   useEffect(() => {
     const fetchTodos = async () => {
       try {
         const todoData = await getTodos();
         setTodos(todoData);
+        setDiscriptionState(new Array(todoData.length).fill(false))
       } catch (e) {
         console.error('Error while fetching todos:', e);
       }
@@ -19,13 +21,23 @@ const App = () => {
     fetchTodos();
   }, []);
 
-
+  const showDiscription = (index: number) => {
+    const newDiscriptionState: Boolean[] = [...discriptionState];
+    newDiscriptionState[index] = !newDiscriptionState[index];
+    setDiscriptionState(newDiscriptionState);
+  }
   return (
     <div className="container">
       <h1>ToDo List</h1>
       <ul>
-        {todos.map((todo: Todo) => (
-          <li className={todo.finished ? "done" : ""} key={todo.id}>{todo.title}</li>
+        {todos.map((todo: Todo, index: number) => (
+          <li className={todo.finished ? "done" : ""} key={todo.id} onClick={() => {showDiscription(index)}}>
+            {todo.title}
+            {discriptionState[index] ? 
+            <div>
+              {todo.description?? "詳細なし"}
+            </div> :  ""}
+          </li>
         ))}
       </ul>
     </div>
