e28bb08a56d627b1218de41bb9fdba2acac1bf04
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2024-07-27 19:06:07 +0900

    apply white space pre wrap

--- frontend/src/component/Task.tsx
+++ frontend/src/component/Task.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Todo from "../types/Todo";
 import { updateTodo } from "../module/api";
 import { UpdateTodo } from "../types/UpdateTodo";
+import React from "react";
 
 type TaskProps = {
   todo: Todo
@@ -37,19 +38,21 @@ const Task = (props: TaskProps) => {
   }
 
   return (
-    <li className="mb-2 flex justify-start border rounded" key={updateState.id}>
-      <input type="checkbox" className="m-2 w-4 h-4" onClick={handleFinished} checked={Boolean(updateState.finished)}/>
-      <div className="flex flex-col">
-        {isEdit ? 
-        <input name="title" value={updateState.title} onChange={handleChange} /> : 
-        <div className="flex justify-start text-xl text-bold">{updateState.title}</div>
-        }
-        {isEdit ? 
-        <textarea name="description" onChange={handleChange}>{updateState.description}</textarea> : 
-        <div>
-          {updateState.description ? updateState.description : ""}
+    <li className="mb-2 flex justify-between border rounded" key={updateState.id}>
+      <div className="flex">
+        <input type="checkbox" className="m-2 w-4 h-4" onClick={handleFinished} checked={Boolean(updateState.finished)}/>
+        <div className="flex flex-col">
+          {isEdit ? 
+          <input name="title" value={updateState.title} onChange={handleChange} /> : 
+          <div className="flex justify-start text-xl text-bold">{updateState.title}</div>
+          }
+          {isEdit ? 
+          <textarea name="description" onChange={handleChange}>{updateState.description}</textarea> : 
+          <div className="align-left whitespace-pre-wrap">
+            {updateState.description ? updateState.description : ""}
+          </div>
+          }
         </div>
-        }
       </div>
       <div className="flex items-center">
         <button onClick={editHandler}>{isEdit ? "保存する" : "編集する"}</button>
