ee14952637db3e73763d808bbfbc1ce1c3580bee
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2024-07-27 19:26:02 +0900

    create delete api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\TodoController;
 Route::get('/todos', [TodoController::class, "index"]);
 Route::post("/todos", [TodoController::class, "create"]);
 Route::put("/todos", [TodoController::class, "update"]);
+Route::delete("/todos", [TodoController::class, "delete"]);
